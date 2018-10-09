d2d292d480cfb9fa20a4de9a5ee09819ae21246b
Bjornhona <asaeri3@hotmail.com>
2018-10-09 15:46:09 +0200

    estamos en ello

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
 class SortedList {
-  constructor() {}
-  add(item) {}
-  get(pos) {}
-  max() {}
-  min() {}
+  constructor() {
+
+    this.items = [];
+    this.length = 0;
+  }
+
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+  }
+
+  get(pos) {
+    return this.items[pos-1];
+  }
+
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      this.items.sort(function(a, b) {
+        return a - b;
+      });
+      return this.items[0];
+    }
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      this.items.sort(function(a, b) {
+        return a - b;
+      });
+      return this.items[this.length-1];
+    }
+  }
   average() {}
   sum() {}
 };
+
+new SortedList();
 
 module.exports = SortedList;
